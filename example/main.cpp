a117a91a166bcf9cbd7b42ff69a17161be111b86
WraxXx <hric.petr@gmail.com>
2019-01-20 13:43:44 +0100

    Example correction

--- example/main.cpp
+++ example/main.cpp
@@ -1,53 +1,56 @@
-#include "cppnetlib.h"
+#include "cppnetlib/cppnetlib.h"
 
 #include <iostream>
 #include <thread>
-#include <mutex>
 
 using namespace cppnetlib;
 
 std::function<void(client::ClientBase<IPProto::TCP>&&, Address&&)> onAccept([](client::ClientBase<IPProto::TCP>&& client, Address&& address) {
-    std::cout << "[ServerSide]: Client " << address.ip() << ":" << address.port() << " connected!" << std::endl;
-
     static const std::string welcomeMessage = "Welcome to cppnetlib server!";
 
-    std::cout << "[ServerSide]: Sending welcome message to " << address.ip() << ":" << address.port() << " ..." << std::endl;
+    std::cout << "[ServerSide]: Client " << address.ip() << ":" << address.port() << " connected!\n"
+        << "[ServerSide]: Sending welcome message to " << address.ip() << ":" << address.port() << " ...\n";
 
-    client.send(reinterpret_cast<const TransmitDataT*>(welcomeMessage.c_str()), welcomeMessage.size());
-
-    std::cout << "[ServerSide]: Welcome message successfuly sent to " << address.ip() << ":" << address.port() << "" << std::endl;
+    const error::ExpectedValue<std::size_t, error::IOReturnValue> sent =
+        client.send(reinterpret_cast<const TransmitDataT*>(welcomeMessage.c_str()), welcomeMessage.size());
+    if (!sent.hasError()) {
+        std::cout << "[ServerSide]: Welcome message successfuly sent to " << address.ip() << ":" << address.port() << "\n";
+    } else {
+        std::clog << "[ServerSide]: Error occured during sending data\n";
+    }
 });
 
 void clientThread() {
     client::Client<IPProto::TCP> client(IPVer::IPv4);
 
-    Address address(IPVer::IPv4, "127.0.0.1", 25565U);
+    const Address address(IPVer::IPv4, "127.0.0.1", 25565U);
 
-    std::cout << "[ClientSide]: Connecting to " << address.ip() << ":" << address.port() << " ..." << std::endl;
+    std::cout << "[ClientSide]: Connecting to " << address.ip() << ":" << address.port() << " ...\n";
 
-    client.connect(Address{IPVer::IPv4, "127.0.0.1", 25565U});
+    client.connect(address);
 
     char dataBuffer[1024] = {};
-    std::size_t received = client.receive(reinterpret_cast<TransmitDataT*>(dataBuffer), sizeof(dataBuffer) - 1U).value();
-
-    std::cout << "[ClientSide]: Received message (" << received << ") - " << dataBuffer << std::endl;
+    const error::ExpectedValue<std::size_t, error::IOReturnValue> received =
+        client.receive(reinterpret_cast<TransmitDataT*>(dataBuffer), sizeof(dataBuffer) - 1U).value();
+    if (!received.hasError()) {
+        std::cout << "[ClientSide]: Received message (Length: " << received.value() << ") - " << dataBuffer << '\n';
+    } else {
+        std::clog << "[ClientSide]: Error occured during receiving data\n";
+    }
 }
 
 int main() {
     server::Server<IPProto::TCP> server(IPVer::IPv4);
-
-    server.bind({IPVer::IPv4, "127.0.0.1", 25565U});
+    server.bind({ IPVer::IPv4, "127.0.0.1", 25565U });
     server.listen(255U);
 
-    std::thread t = std::thread(clientThread);
+    std::cout << "[ServerSize]: Ready. Waiting for incomming connection ...\n";
 
-    std::cout << "[ServerSize]: Ready. Waiting for incomming connection ..." << std::endl;
+    std::thread t = std::thread(clientThread);
 
     server.tryAccept(onAccept);
 
     t.join();
-
-    std::cin.get();
 
     return 0;
 };
